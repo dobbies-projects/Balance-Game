97fe63dac4ac8a063c461c66bc89afae42b5653e
seon <sunju701@gmail.com>
2024-05-02 15:53:58 +0900

    feat: Home UI

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,12 +6,16 @@ const Home = () => {
   return (
     <div className={global.container}>
       <div className={styles.title}>밸런스 게임</div>
-      <div>당신의 직업을 골라주세요!</div>
+      <div className={styles.text}>당신의 직업을 골라주세요!</div>
       <div>
-        <Link to={`game/1`}>대학생</Link>
+        <Link to={`game/1`} style={{ textDecoration: 'none' }}>
+          <button className={styles.buttonStudent}> 대학생 </button>
+        </Link>
       </div>
       <div>
-        <Link to={`game/2`}>직장인</Link>
+        <Link to={`game/2`} style={{ textDecoration: 'none' }}>
+          <button className={styles.buttonWorker}> 직장인 </button>
+        </Link>
       </div>
     </div>
   );
